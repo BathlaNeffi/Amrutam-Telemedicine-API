0fdde68b0dd266928a90949e7c8f7489cf141e16
BathlaNeffi <123646861+BathlaNeffi@users.noreply.github.com>
2025-11-25 15:30:58 +0530

    second commit

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,13 +1,17 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging:false, 
-     pool: {
+    dialect: "postgres",
+    logging: false,
+    pool: {
       max: 10,
       min: 1,
       acquire: 30000,
@@ -15,6 +19,7 @@ const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.
     }
   }
 );
+
 
 // Simple function to test the connection
 async function connectDB() {
